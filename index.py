31ffb4da7425128536c13b38b3342a2917b5e18a
Gil Miranda <gmiranda540@yahoo.com>
2024-10-10 23:43:33 -0400

    First upload of program

---
+++ index.py
@@ -0,0 +1,68 @@
+questionNumberOne = "What is the name for a fear of spiders?"
+answerOne = {
+  "A": "Aquaphobia",
+  "B": "Mysophobia",
+  "C": "Claustrophobia",
+  "D": "Arachnophobia"
+}
+print(answerOne)
+if input(questionNumberOne).lower() == "d":
+    print("correct")
+else:
+    print("incorrect")
+
+
+questionNumberTwo = "Who is the first person to step on the moon?"
+answerTwo = {
+  "A": "Neil Armstrong",
+  "B": "Yuri Gagarin",
+  "C": "Alan Shepard",
+  "D": "John Glen"
+}
+print(answerTwo)
+if input(questionNumberTwo).lower() == "a":
+    print("correct")
+else:
+    print("incorrect")
+
+
+questionNumberThree = "Who is the first person to sail around the world?"
+answerThree = {
+  "A": "Christopher Columbus",
+  "B": "Zheng He",
+  "C": "Ferdinand Magellan",
+  "D": "John Cabot"
+}
+print(answerThree)
+if input(questionNumberThree).lower() == "c":
+    print("correct")
+else:
+    print("incorrect")
+
+
+questionNumberFour = "How many days are in a leap year?"
+answerFour = {
+  "A": "1000",
+  "B": "275",
+  "C": "365",
+  "D": "366"
+}
+print(answerFour)
+if input(questionNumberFour).lower() == "d":
+    print("correct")
+else:
+    print("incorrect")
+
+
+questionNumberFive = "What mammal lays eggs?"
+answerFive = {
+  "A": "Platypuses",
+  "B": "Sharks",
+  "C": "Humans",
+  "D": "Dolphins"
+}
+print(answerFive)
+if input(questionNumberFive).lower() == "a":
+    print("correct")
+else:
+    print("incorrect")
